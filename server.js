574f9fcb423e294c1dba705af0718e62ff73a164
Sandeep Wadhwa <Sandeep Wadhwa>
2016-02-26 12:53:35 +0530

    Added M of MEAN

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus'),
     open = require('open'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -35,6 +36,22 @@ app.get('/partials/:partialPath', function(req, res) {
 app.get('/', function(req, res){
     console.log('main page requested');
     res.render('index');
+});
+
+
+// DB CONNECTION
+var dbName = 'testDb';
+var connString = 'mongodb://localhost/'+dbName;
+mongoose.connect(connString);
+var db = mongoose.connection;
+
+db.on('error', function(err){
+    console.log('Error Connecting to Mongodb '+err);
+    //consol.error.bind(console, 'connection error');
+});
+
+db.once('open', function callback(){
+   console.log('MongoDB Connection Opened'); 
 });
 
 var port = 3000;
